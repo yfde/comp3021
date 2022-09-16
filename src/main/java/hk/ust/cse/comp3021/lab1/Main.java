797c1dd1b7d56e418f2a94126e14ed4413b4ddb7
Yufan Deng <yf-deng@outlook.com>
2022-09-16 22:31:03 +0800

    init

---
+++ src/main/java/hk/ust/cse/comp3021/lab1/Main.java
@@ -0,0 +1,14 @@
+package hk.ust.cse.comp3021.lab1;
+
+public class Main {
+
+    // TODO
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("This is a demo project.");
+    }
+}
+
