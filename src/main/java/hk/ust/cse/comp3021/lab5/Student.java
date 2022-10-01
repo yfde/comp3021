c4f8ffb6178eb73e79dee893da928f7948f3b96c
Yufan Deng <yf-deng@outlook.com>
2022-10-01 22:45:30 +0800

    finish

--- src/main/java/hk/ust/cse/comp3021/lab5/Student.java
+++ src/main/java/hk/ust/cse/comp3021/lab5/Student.java
@@ -4,5 +4,33 @@ public class Student extends Person {
     private int id;
     private String major;
 
-    // TODO
+    public Student(String name, int age, int id, String major) {
+        super(name, age);
+        this.id = id;
+        this.major = major;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", major='" + major + '\'' +
+                '}';
+    }
 }
