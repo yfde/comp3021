564b235af008375feef3ab4a7e580a9e4378c93b
William Aaron Cheung <troublor@live.com>
2022-10-13 14:19:59 +0800

    :bug: change method signature

--- src/main/java/hk/ust/comp3021/gui/component/maplist/MapModel.java
+++ src/main/java/hk/ust/comp3021/gui/component/maplist/MapModel.java
@@ -2,10 +2,15 @@ package hk.ust.comp3021.gui.component.maplist;
 
 import hk.ust.comp3021.game.GameMap;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 /**
  * Data model for storing the metadata of a  game map.
@@ -18,12 +23,15 @@ import java.util.Date;
 public record MapModel(String name, Path file, Date loadAt, GameMap gameMap) {
 
     /**
-     * @param gameMapFile The path to the game map files.
+     * @param gameMapURL The path to the game map files.
      * @return The map model.
      * @throws IOException When fails to access the file.
      */
-    public static MapModel load(Path gameMapFile) throws IOException {
-        var gameMap = GameMap.parse(Files.readString(gameMapFile));
+    public static MapModel load(URL gameMapURL) throws IOException {
+        var gameMapFile = Path.of(gameMapURL.getPath());
+        var reader = new BufferedReader(new InputStreamReader(gameMapURL.openStream()));
+        var content = reader.lines().collect(Collectors.joining("\n"));
+        var gameMap = GameMap.parse(content);
         return new MapModel(gameMapFile.getFileName().toString(), gameMapFile, new Date(), gameMap);
     }
 }
