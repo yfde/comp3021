1fda73bf15eaf89c53ae0fe78bccab2ac5e6a9c1
Yufan Deng <yf-deng@outlook.com>
2022-11-03 23:01:34 +0800

    finish

--- src/main/java/hk/ust/cse/comp3021/lab9/ClassFileParser.java
+++ src/main/java/hk/ust/cse/comp3021/lab9/ClassFileParser.java
@@ -15,6 +15,18 @@ public class ClassFileParser {
      * @return A boolean value indicating whether it is a Java class file.
      */
     public static boolean isClassFile(@NotNull File theFile) {
+        if (!theFile.isFile() || !theFile.getName().endsWith(".class")) {
+            return false;
+        }
+        try (var stream = new FileInputStream(theFile)) {
+            int[] b = {202, 254, 186, 190};
+            for (var i: b) {
+                if (stream.read() != i) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (IOException ignored) {}
         return false;
     }
 
@@ -25,6 +37,13 @@ public class ClassFileParser {
      * @return The major version of the class file (e.g, 55 for Java 11). Return -1 if it is not a class file or the major version can not be found.
      */
     public static int getClassFileVersion(@NotNull File theFile) {
+        if (!isClassFile(theFile)) {
+            return -1;
+        }
+        try (var stream = new FileInputStream(theFile)) {
+            stream.skip(7);
+            return stream.read();
+        } catch (IOException ignored) {}
         return -1;
     }
 
